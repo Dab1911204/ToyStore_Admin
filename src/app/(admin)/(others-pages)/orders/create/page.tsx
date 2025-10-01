63d6ba5e546d2e1a7a690080e840643b2804b82f
Anh <nguyenducanhpx2004@gmail.com>
2025-10-01 22:00:31 +0700

    jsjs

--- src/app/(admin)/(others-pages)/orders/create/page.tsx
+++ src/app/(admin)/(others-pages)/orders/create/page.tsx
@@ -1,6 +1,5 @@
+import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import AddForm from "@/components/form/Orders/AddForm";
-import { FormProvider } from "@/context/FormContext";
 import { Metadata } from "next";
 import React from "react";
 
@@ -12,9 +11,14 @@ export default function CreateOrdersPage() {
   return (
     <div>
       <PageBreadcrumb pageTitle="Thêm đơn hàng" pageParent="Quản lý đơn hàng" urlPageParent="/orders" />
-      <FormProvider >
-        <AddForm />
-      </FormProvider>
+      <div className="grid grid-cols-2 gap-4">
+        <ComponentCard title="Danh sách tin tức" isDelete={false} linkBtn="news" titleBtn="Tin Tức" isAdd={false}>
+          sjjsjs
+        </ComponentCard>
+        <div className="">
+          hshsh
+        </div>
+      </div>
     </div>
   );
 }
