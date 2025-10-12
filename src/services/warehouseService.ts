12c96d32eb3d8166165e52c1aca76724d779836b
NgocAnh <lethingocanhh2808@gmail.com>
2025-10-12 22:52:22 +0700

    push deletewarehouse

--- src/services/warehouseService.ts
+++ src/services/warehouseService.ts
@@ -1,8 +1,9 @@
 
 import { WarehousesResType } from "@/schemaValidations/warehouse.schema";
-import {del, get, post, put} from "@/utils/request";
+import {del, get} from "@/utils/request";
 
 export const WarehouseService = {
   getListWarehouse:(url:string)=>get<WarehousesResType>(url,{requireAuth:true}),
+  deleteWarehouse:(id:string)=>del<WarehousesResType>(`/api/Warehouse`,`${id}?force=true`,{requireAuth:true}),
 };
 
